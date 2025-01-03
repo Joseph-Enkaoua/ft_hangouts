9fe7e91b055a30a5c16d2ed96814b6b1ed122545
Joseph Enkaoua <joseph.enkaoua@gmail.com>
2025-01-03 21:38:27 +0100

    Initial Commit

---
+++ hangouts/hangoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hangoutsApp.swift
+//  hangouts
+//
+//  Created by Joseph Enkaoua on 03.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct hangoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
